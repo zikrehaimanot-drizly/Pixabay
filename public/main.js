22d25ba4861584f7d33e1cf405a87a9e8e83b85a
Zikre Haimanot <zikrehaimanot@gmail.com>
2023-07-24 22:43:48 -0400

    ZH [prep for debrief, understand why -2 works

--- public/main.js
+++ public/main.js
@@ -4,10 +4,11 @@ const prevResultsButton = document.getElementById('prevPageOfResults');
 const imageContainer = document.getElementById('imageContainer');
 let previousSearchTerm = '';
 let previousLanguage = '';
-let pageCount = 1;
+var pageCount = 0;
 
 function fetchAndRenderImages(searchTerm, selectedLanguage) {
-  console.log(`${pageCount} thisi spage count`)
+  pageCount += 1;
+  console.log(`${pageCount} this is page count when we make the call`)
   fetch(`/search?term=${encodeURIComponent(searchTerm)}&page=${pageCount}&lang=${selectedLanguage}`)
     .then(response => {
       if (!response.ok) {
@@ -16,7 +17,7 @@ function fetchAndRenderImages(searchTerm, selectedLanguage) {
       return response.json();
     })
     .then(data => {
-      pageCount += 1;
+      
       console.log(data);
       const images = data.hits;
 
@@ -42,6 +43,7 @@ function fetchAndRenderImages(searchTerm, selectedLanguage) {
           });
           liElement.appendChild(imgElement);
           imageContainer.appendChild(liElement);
+          console.log(`${pageCount} after we've displayed images after fetch`)
         });
       }
     })
@@ -56,10 +58,12 @@ function handleSearch() {
   const searchTerm = document.getElementById('input').value;
   const selectedLanguage = document.getElementById('languageDropdown').value;
 
-  if (searchTerm !== previousSearchTerm || selectedLanguage !== previousLanguage) {
-    pageCount = 1;
-  }
+  //this might need to be AND instead of OR
 
+  if (searchTerm !== previousSearchTerm || selectedLanguage !== previousLanguage) {
+    pageCount = 0;
+  }
+  console.log(`${pageCount} right before we call fetchimage during more pic call`)
   fetchAndRenderImages(searchTerm, selectedLanguage);
 
   previousSearchTerm = searchTerm;
@@ -72,7 +76,9 @@ function handlePrevSearch() {
   //this will be last commit and includes minor css changes and trying to debug this edge case when i added the prevsearch button
   const searchTerm = document.getElementById('input').value;
   const selectedLanguage = document.getElementById('languageDropdown').value;
+  console.log(`${pageCount} right before we atttempt subtracting`)
   pageCount = pageCount - 2
+  console.log(`${pageCount}, this is count after we've subtracted from count`)
   fetchAndRenderImages(searchTerm, selectedLanguage);
  
 
